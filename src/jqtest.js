c4597da86aad59dcd686d41f0fcabc55417a0f92
FredericoGon <fredericogon@gmail.com>
2018-07-24 15:37:45 -0300

    create

--- src/jqtest.js
+++ src/jqtest.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
   $("#jqbutton").click(function(){
-      $("#jqimg").hide();
+    var x = Math.floor((Math.random() * 119) + 1);
+    $("#jqimg").attr({
+      "src" : rares[x]
   });
+});
+$("#createButton").click(function(){
+  var x = Math.floor((Math.random() * 119) + 1);
+  var img1 = $("<img></img>").attr({
+    "src" : rares[x]
+    });
+    $("#createDiv").append(img1);
+});
 });
