fc0b4d7a1e776dc6641fe4890f0666042b7c92b3
FredericoGon <fredericogon@gmail.com>
2018-08-06 20:43:12 -0300

    cores

--- src/jqtest.js
+++ src/jqtest.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
+// Teste para usar Jquery para trocar imagem
   $("#jqbutton").click(function(){
     var x = Math.floor((Math.random() * 119) + 1);
     $("#jqimg").attr({
       "src" : rares[x]
   });
 });
+// Teste para usar Jquery para criar uma imagem
 $("#createButton").click(() => {
   var x = Math.floor((Math.random() * 119) + 1);
   var img1 = $("<img>").attr({
@@ -13,11 +15,15 @@ $("#createButton").click(() => {
     $("#createDiv").append(img1);
     $("#createButton").remove();
 });
+
+//   Teste com a Array mais complexa
+
 $("#createButton2").click(()=>{
   var arrayRare = arrayTest.filter(carta => carta.rarity == "rare")
   var max = arrayRare.length
   var x = Math.floor((Math.random() * max));
   var img2 = $("<img>").attr({
+    "id" : "firstCard",
     "src" : arrayRare[x].source
     });
     $("#createDiv2").append(img2);
@@ -27,6 +33,7 @@ $("#createButton2").click(()=>{
   var max = arrayUncommon.length
   var x = Math.floor((Math.random() * max));
   var img3 = $("<img>").attr({
+  "id" : "secondCard",
   "src" : arrayUncommon[x].source
   });
   $("#createDiv2").append(img3);
@@ -35,12 +42,26 @@ $("#createButton2").click(()=>{
   var max = arrayCommon.length
   var x = Math.floor((Math.random() * max));
   var img4 = $("<img>").attr({
+  "id" : "thirdCard",
   "src" : arrayCommon[x].source
   });
   $("#createDiv2").append(img4);
 });
-$("#colors").click(()=>{
-  // aqui vai a função para organizar o booster por cores!!
-})
 
+// Teste para organizar as cores
+
+$("#colors").click(()=>{
+  var images = [
+    "http://mythicspoiler.com/m19/cards/woodlandstream1.jpg",
+    "http://mythicspoiler.com/m19/cards/totallylost.jpg"
+  ];
+  images.push(("#thirdCard").src);
+  //images.push(img3);
+  //images.push(img4);
+  //images.sort();
+  var imgA = $("<img>").attr({
+    "src" : images[2]
+    });
+  $("#createDiv3").append(imgA);
+})
 });
