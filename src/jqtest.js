47d4fbfc92c02134f1facd86341eed2a7be4b666
FredericoGon <fredericogon@gmail.com>
2018-08-08 18:41:24 -0300

    booster

--- src/jqtest.js
+++ src/jqtest.js
@@ -57,7 +57,9 @@ var imgC1 = $("<img>").attr({
   "src" : booster[0].source
   });
   $("#createBoosterDiv").append(imgC1);
-//card 2 Uncommon
+
+//Array com todas as Uncommon
+
 var arrayUncommon = arrayTest.filter(carta => carta.rarity == "uncommon")
 var max = arrayUncommon.length
 var x = Math.floor((Math.random() * max)); // randomizando o que vai gerar a primeira carta
@@ -71,38 +73,81 @@ while(booster.length != 4){ // Mandando gerar cartas até chegar à 4 cartas no 
   }
   booster.push(arrayUncommon[x])
 }
+
+//card 2 Uncommon
+
 var imgC2 = $("<img>").attr({
   "src" : booster[1].source
   });
   $("#createBoosterDiv").append(imgC2);
 
-
-
-
-
 //card 3 Uncommmon
-// var arrayUncommon = arrayTest.filter(carta => carta.rarity == "uncommon")
-// var max = arrayUncommon.length
-// var x = Math.floor((Math.random() * max));
-// booster.push(arrayUncommon[x])
+
 var imgC3 = $("<img>").attr({
   "src" : booster[2].source
   });
   $("#createBoosterDiv").append(imgC3);
+
 //card 4 Uncommmon
-// var arrayUncommon = arrayTest.filter(carta => carta.rarity == "uncommon")
-// var max = arrayUncommon.length
-// var x = Math.floor((Math.random() * max));
-// booster.push(arrayUncommon[x])
+
 var imgC4 = $("<img>").attr({
-  "src" : booster[2].source
+  "src" : booster[3].source
   });
   $("#createBoosterDiv").append(imgC4);
+
+
+//Array com todas as Common
+
+var arrayCommon = arrayTest.filter(carta => carta.rarity == "common")
+var max = arrayCommon.length
+var x = Math.floor((Math.random() * max)); // randomizando o que vai gerar a primeira carta
+booster.push(arrayCommon[x]) // Gerando a primeira carta incomum, que é o item 1 do booster
+while(booster.length != 9){ // Mandando gerar cartas até chegar à 4 cartas no booster
+  max = arrayCommon.length
+  x = Math.floor((Math.random() * max)); // randomizando as próximas cartas
+  while(booster.find(carta => carta.name == arrayCommon[x].name)){ //compara
+  max = arrayCommon.length
+  x = Math.floor((Math.random() * max));
+  }
+  booster.push(arrayCommon[x])
+}
+
+//card 5 Common
+
+var imgC5 = $("<img>").attr({
+  "src" : booster[4].source
+  });
+  $("#createBoosterDiv").append(imgC5);
+
+//card 6 Common
+
+var imgC6 = $("<img>").attr({
+  "src" : booster[5].source
+  });
+  $("#createBoosterDiv").append(imgC6);
+
+//card 7 Common
+
+var imgC7 = $("<img>").attr({
+  "src" : booster[6].source
+  });
+  $("#createBoosterDiv").append(imgC7);
+
+//card 8 Common
+
+var imgC8 = $("<img>").attr({
+  "src" : booster[7].source
+  });
+  $("#createBoosterDiv").append(imgC8);
+
+//card 9 Common
+
+var imgC9 = $("<img>").attr({
+  "src" : booster[8].source
+  });
+  $("#createBoosterDiv").append(imgC9);
+
 })
-//card 5 Commmon
-
-
-
 // Teste para organizar as cores
 
 $("#colors").click(()=>{
